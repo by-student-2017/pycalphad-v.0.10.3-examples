41659acecb7d5f3b474c72c823c63c7c7c3fc30a
by-student-2017 <studentsctest@gmail.com>
2023-12-07 15:55:48 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ Al-Mg/binary-AlMg.py
@@ -0,0 +1,16 @@
+#%matplotlib inline
+import matplotlib.pyplot as plt
+from pycalphad import Database, binplot
+import pycalphad.variables as v
+
+# Load database 
+tdb = Database('Al-Mg_Zhong.tdb')
+# Define the components
+comps = ['AL', 'MG', 'VA']
+# Get all possible phases programmatically
+phases = tdb.phases.keys()
+
+# Plot the phase diagram, if no axes are supplied, a new figure with axes will be created automatically
+binplot(tdb, comps, phases, {v.N: 1, v.P:101325, v.T: (300, 2500, 10), v.X('MG'):(0, 1, 0.02)})
+
+plt.show()
