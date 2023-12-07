0034e81b30238254a8edda3ec4082c7400e855ee
by-student-2017 <studentsctest@gmail.com>
2023-12-07 15:28:22 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ Cr-Fe-Ti/ternary-CrFeTi.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from pycalphad import Database, ternplot
+from pycalphad import variables as v
+
+tdb = Database('crfeti_wan.tdb') # You can get 'alcrni_dup.tdb' file from CPDDB.
+comps = ['CR', 'FE', 'TI', 'VA']
+phases = list(tdb.phases.keys())
+conds = {v.T: 1073, v.P:101325, v.X('CR'): (0,1,0.015), v.X('FE'): (0,1,0.015)}
+
+ternplot(tdb, comps, phases, conds, x=v.X('CR'), y=v.X('FE'))
+
+plt.show()
