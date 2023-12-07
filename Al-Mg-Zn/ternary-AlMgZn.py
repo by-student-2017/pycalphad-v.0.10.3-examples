785ed2fa1efaf205dea90c1f65dcf103a8a8c628
by-student-2017 <studentsctest@gmail.com>
2023-12-07 14:26:17 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ Al-Mg-Zn/ternary-AlMgZn.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from pycalphad import Database, ternplot
+from pycalphad import variables as v
+
+db_al_cu_y = Database('cost507R.TDB')
+comps = ['AL', 'MG', 'ZN', 'VA']
+phases = list(db_al_cu_y.phases.keys())
+conds = {v.T: 608, v.P:101325, v.X('MG'): (0,1,0.015), v.X('ZN'): (0,1,0.015)}
+
+ternplot(db_al_cu_y, comps, phases, conds, x=v.X('MG'), y=v.X('ZN'))
+
+plt.show()
+
---
+++ Cr-Fe-Ni/ternary-CrFeNi.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from pycalphad import Database, ternplot
+from pycalphad import variables as v
+
+tdb = Database('saf2507.TDB')
+comps = ['CR', 'FE', 'NI', 'VA']
+phases = list(tdb.phases.keys())
+conds = {v.T: 1200, v.P:101325, v.X('CR'): (0,1,0.015), v.X('NI'): (0,1,0.015)}
+
+ternplot(tdb, comps, phases, conds, x=v.X('CR'), y=v.X('NI'))
+
+plt.show()
