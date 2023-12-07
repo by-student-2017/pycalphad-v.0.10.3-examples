777e02711afe33087c816c57a721b532981a73a4
by-student-2017 <studentsctest@gmail.com>
2023-12-07 15:41:09 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ Al-Cu-Y/ternary-AlCuY-enthalpy.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from pycalphad import Database
+from pycalphad.plot import triangular
+from pycalphad import calculate
+
+db_al_cu_y = Database('Al-Cu-Y.tdb')
+comps = ['AL', 'CU', 'Y', 'VA']
+
+# some sample data, these could be from an equilibrium calculation or a property model.
+# here we are calculating the mixing enthlapy of the FCC_A1 phase at 830K. 
+c = calculate(db_al_cu_y, comps, 'FCC_A1', output='HM_MIX', T=830, P=101325, pdens=5000)
+
+# Here we are getting the values from our plot. 
+xs = c.X.values[0, 0, 0, :, 0]  # 1D array of Al compositions
+ys = c.X.values[0, 0, 0, :, 1]  # 1D array of Cu compositions
+zs = c.HM_MIX.values[0, 0, 0, :]  # 1D array of mixing enthalpies at these compositions
+
+# when we imported the pycalphad.plot.triangular module, it made the 'triangular' projection available for us to use.
+fig = plt.figure()
+ax = fig.add_subplot(projection='triangular')
+ax.scatter(xs, ys, c=zs, 
+           cmap='coolwarm', 
+           linewidth=0.0)
+
+# label the figure
+ax.set_xlabel('X (AL)')
+ax.set_ylabel('X (CU)')
+ax.yaxis.label.set_rotation(60)  # rotate ylabel
+ax.yaxis.set_label_coords(x=0.12, y=0.5)  # move the label to a pleasing position
+ax.set_title('AL-CU-Y HM_MIX at 830K')
+
+# set up the colorbar
+cm = plt.cm.ScalarMappable(cmap='coolwarm')
+cm.set_array(zs)
+fig.colorbar(cm);
+
+plt.show()
