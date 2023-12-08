b33383529814ffa62eefb0a46bd347a1621e1816
by-student-2017 <studentsctest@gmail.com>
2023-12-08 19:29:16 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ Fe-Nb-Ni_test/ternary-FeNbNi.py
@@ -0,0 +1,36 @@
+#%matplotlib inline # for Jupyter notebooks
+import matplotlib.pyplot as plt
+from pycalphad import Database, ternplot
+from pycalphad import variables as v
+
+#------User input area--------
+#tdb_file = 'Enriched dataseTDB_V2.txt' # from TDBDB
+tdb_file = 'CORRECTdatabase.txt' # from TDBDB
+ELA = 'Ni'  # x-axis
+ELB = 'Fe' # lower left side
+ELC = 'Nb' # y-axis
+Temp = 1373 # [K]
+#------User input area--------
+# It is preferable to specify elements in "ELA, ELB and ELC" in alphabetical order.
+
+img_comps = ELA+ELB+ELC
+
+ELA = ELA.upper ()
+ELB = ELB.upper ()
+ELC = ELC.upper ()
+
+tdb = Database(tdb_file)
+comps = [ELA, ELB, ELC, 'VA']
+phases = list(tdb.phases.keys())
+conds = {v.T: Temp, v.P:101325, v.X(ELA): (0,1,0.015), v.X(ELC): (0,1,0.015)}
+
+ternplot(tdb, comps, phases, conds, x=v.X(ELA), y=v.X(ELC))
+
+plt.text(-0.15, -0.15, ELB, family='monospace', fontsize=20)
+plt.text(-0.08, 1.08, str(Temp)+' K', family='monospace', fontsize=20)
+
+plt.rcParams['savefig.bbox'] = 'tight'
+output_file = 'ternary-'+img_comps+"-"+str(Temp)+'K.png'
+plt.savefig(output_file)
+
+plt.show()
