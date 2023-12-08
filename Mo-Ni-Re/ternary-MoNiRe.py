d856a0ff5f11967f9d336952bfadcb73d413e91f
by-student-2017 <studentsctest@gmail.com>
2023-12-08 11:45:56 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ Mo-Ni-Re/ternary-MoNiRe.py
@@ -0,0 +1,29 @@
+#%matplotlib inline # for Jupyter notebooks
+import matplotlib.pyplot as plt
+from pycalphad import Database, ternplot
+from pycalphad import variables as v
+
+#------User input area--------
+tdb_file = 'mmc1.TDB' # from TDBDB (https://avdwgroup.engin.brown.edu/)
+ELA = 'Re' # x-axis
+ELB = 'Mo' # lower left side
+ELC = 'Ni' # y-axis
+Temp = 500 # [K]
+#------User input area--------
+# It is preferable to specify elements in "ELA, ELB and ELC" in alphabetical order.
+
+ELA = ELA.upper ()
+ELB = ELB.upper ()
+ELC = ELC.upper ()
+
+tdb = Database(tdb_file)
+comps = [ELA, ELB, ELC, 'VA']
+phases = list(tdb.phases.keys())
+conds = {v.T: Temp, v.P:101325, v.X(ELA): (0,1,0.015), v.X(ELC): (0,1,0.015)}
+
+ternplot(tdb, comps, phases, conds, x=v.X(ELA), y=v.X(ELC))
+
+plt.text(-0.1, -0.1, ELB, family='monospace', fontsize=20)
+plt.text(-0.1, 1.1, str(Temp)+' K', family='monospace', fontsize=20)
+
+plt.show()
