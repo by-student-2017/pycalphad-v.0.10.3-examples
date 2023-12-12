32d6a920d796eb0ea7727a406a5e578038b23e87
by-student-2017 <studentsctest@gmail.com>
2023-12-12 12:00:00 +0900

    WSL2(ubuntu 22.04 LTS) on win11

---
+++ test/Nb-Ni-Si/ternary-NbNiSi.py
@@ -0,0 +1,35 @@
+#%matplotlib inline # for Jupyter notebooks
+import matplotlib.pyplot as plt
+from pycalphad import Database, ternplot
+from pycalphad import variables as v
+
+#------User input area--------
+tdb_file = 'dos_2015.tdb' # from TDBDB (dos Santos et al. (2015))
+ELA = 'Nb' # x-axis
+ELB = 'Ni' # lower left side
+ELC = 'Si'  # y-axis
+Temp = 1073 # [K]
+#------User input area--------
+# It is preferable to specify elements in "ELA, ELB and ELC" in alphabetical order.
+
+img_comps = ELA+ELB+ELC
+
+ELA = ELA.upper ()
+ELB = ELB.upper ()
+ELC = ELC.upper ()
+
+tdb = Database(tdb_file)
+comps = [ELA, ELB, ELC, 'VA']
+phases = list(tdb.phases.keys())
+conds = {v.T: Temp, v.P:101325, v.X(ELA): (0,1,0.015), v.X(ELC): (0,1,0.015)}
+
+ternplot(tdb, comps, phases, conds, x=v.X(ELA), y=v.X(ELC))
+
+plt.text(-0.15, -0.15, ELB, family='monospace', fontsize=20)
+plt.text(-0.08, 1.08, str(Temp)+' K', family='monospace', fontsize=20)
+
+plt.rcParams['savefig.bbox'] = 'tight'
+output_file = 'ternary-'+img_comps+"-"+str(Temp)+'K.png'
+plt.savefig(output_file)
+
+plt.show()
